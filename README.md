e92f73470a096e91d3188aa9995f8a649b7b679f
Abdullah-Sheehan <abdshe2005@gmail.com>
2023-09-28 02:10:15 +0600

    validated

---
+++ README.md
@@ -0,0 +1,1 @@
+# An app which will validate an expression whether it is correct or not.
---
+++ scripts.js
@@ -0,0 +1,35 @@
+let valid = document.querySelector(".alert-success").classList;
+let invalid = document.querySelector(".alert-danger").classList;
+document.getElementById("mail").addEventListener("click", () => {
+	valid.add("d-none");
+	invalid.add("d-none");
+	let input = prompt("Enter your Email:");
+	let regex = /^([a-zA-Z0-9]\.?)+([^\.])@([a-zA-Z0-9]\.?)+([^\.])$/;
+	if (regex.test(input)) {
+		valid.remove("d-none");
+	} else {
+		invalid.remove("d-none");
+	}
+});
+document.getElementById("post").addEventListener("click", () => {
+	valid.add("d-none");
+	invalid.add("d-none");
+	let input = prompt("Enter your Post Code:");
+	let regex = /^[0-9]{4}$/;
+	if (regex.test(input)) {
+		valid.remove("d-none");
+	} else {
+		invalid.remove("d-none");
+	}
+});
+document.getElementById("phone").addEventListener("click", () => {
+	valid.add("d-none");
+	invalid.add("d-none");
+	let input = prompt("Enter your Phone Number:");
+    let regex = /^(\+)?(88)?01[0-9]{9}$/;
+	if (regex.test(input)) {
+		valid.remove("d-none");
+	} else {
+		invalid.remove("d-none");
+	}
+});
